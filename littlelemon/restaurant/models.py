b3e33a45fe7444ec7068d7720ce311127b5196b3
Dale <dale.a.lenze@gmail.com>
2025-07-14 15:32:03 -0500

    Added MySQL and 2 Models for the application
    Migrated 2 models which included the Booking model and the Menu model.
    Updated the application admin.py file to reflect the new models in the admin
    interface as well as altered the settings.py file at the project level to
    include the use of MySQL with username and password.

--- littlelemon/restaurant/models.py
+++ littlelemon/restaurant/models.py
@@ -1,3 +1,29 @@
 from django.db import models
+from django.core.validators import MaxValueValidator, MinValueValidator
 
 # Create your models here.
+class Booking(models.Model):
+    Name = models.CharField(max_length=255)
+    No_of_guests = models.IntegerField(
+        default = 1,
+        validators=[
+            MinValueValidator(1),
+            MaxValueValidator(6),
+        ]
+    )
+    Booking_date = models.DateField()
+
+    def __str__(self):
+        return self.Name + str(self.Booking_date)
+
+class Menu(models.Model):
+    Title = models.CharField(max_length=255)
+    Price = models.DecimalField(max_digits=10, decimal_places=2)
+    Inventory = models.IntegerField(
+        validators=[
+            MinValueValidator(1),
+        ]
+    )
+
+    def __str__(self):
+        return self.Title
