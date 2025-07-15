da30f123b1395f412143f16e00938efe362eb1a3
Dale <dale.a.lenze@gmail.com>
2025-07-15 10:36:37 -0500

    Added API for menu items and configured URLS
    Used DefaultRouter() for users view accessed at /restaurant/users.
    Also added generics api view for menu items as well as single menu
    items which are found by appending their corresponding ID to the
    end of the URL.
    
    Fixed error in serializers.py in restaurant app that was preventing
    the /users endpoint from populating.
    	Description: 'url' was not an available field name.

--- littlelemon/restaurant/urls.py
+++ littlelemon/restaurant/urls.py
@@ -1,8 +1,13 @@
-from django.urls import path
+from django.urls import path, include
 from . import views
+from rest_framework import routers
+
+router = routers.DefaultRouter()
+router.register(r'users', views.UserViewSet, basename="users")
 
 urlpatterns = [
-
-    path('', views.index, name='index'),
-
+    path('', include(router.urls)),
+    path('index', views.index, name="index"),
+    path('menu/', views.MenuItemView.as_view(), name="menu"),
+    path('menu/<int:pk>', views.SingleMenuItemView.as_view(), name="single-menu-item-view"),
 ]
--- littlelemon/restaurant/views.py
+++ littlelemon/restaurant/views.py
@@ -1,5 +1,23 @@
 from django.shortcuts import render
 
+from rest_framework.response import Response
+from rest_framework import viewsets, generics
+from django.contrib.auth.models import User
+from .models import Menu
+from .serializers import UserSerializer, MenuSerializer
+
 # Create your views here.
 def index(request):
     return render(request, 'index.html', {})
+
+class UserViewSet(viewsets.ModelViewSet):
+    queryset = User.objects.all()
+    serializer_class = UserSerializer
+
+class MenuItemView(generics.ListCreateAPIView):
+    queryset = Menu.objects.all()
+    serializer_class = MenuSerializer
+
+class SingleMenuItemView(generics.RetrieveUpdateAPIView, generics.DestroyAPIView):
+    queryset = Menu.objects.all()
+    serializer_class = MenuSerializer
