46f0464c3c2948aa11dfa34044f4c2045c7a6ee6
Dale <dale.a.lenze@gmail.com>
2025-07-15 17:02:58 -0500

    Unit tests within the tests.py file in the app folder.

--- littlelemon/restaurant/tests.py
+++ littlelemon/restaurant/tests.py
@@ -1,4 +1,27 @@
 from django.test import TestCase
-from .models import Menu
+from restaurant.models import Menu
+from decimal import Decimal
 # Create your tests here.
 
+class MenuItemTest(TestCase):
+    def test_get_item(self):
+        item = Menu.objects.create(Title="IceCream", Price=Decimal('80.00'), Inventory=100)
+        item_str = str(item)
+
+        item2 = Menu.objects.create(Title="Mushroom Soup", Price=Decimal('13.50'), Inventory=100)
+        item2_str = str(item2)
+
+        self.assertEqual(item_str, "IceCream : 80.00")
+        self.assertEqual(item2_str, "Mushroom Soup : 13.50")
+
+class MenuViewTest(TestCase):
+    def setUp(self):
+        self.obj1 = Menu.objects.create(Title="Brownie", Price=Decimal("5.00"), Inventory=35)
+        self.obj2 = Menu.objects.create(Title="Bone Soup", Price=Decimal("9.00"), Inventory=45)
+
+    def test_get_all(self):
+        menu_items = Menu.objects.all()
+
+        self.assertEqual(menu_items.count(), 2)
+        self.assertEqual(menu_items[0].Title, "Brownie")
+        self.assertEqual(menu_items[1].Title, "Bone Soup")
